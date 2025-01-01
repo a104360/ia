312afcddc85d424b79119d3cb4ff874f2d4507fb
QGMs <jdospmanos@gmail.com>
2025-01-01 21:02:04 +0000

    probabilidade clima

--- software/Entidades/Clima.py
+++ software/Entidades/Clima.py
@@ -1,5 +1,5 @@
 class Clima:
-    def __init__(self, tipo, id = -1, bloqueante = False, probabilidade = 0):
+    def __init__(self, tipo, id = -1, probabilidade = 0):
         """
         Classe base para representar diferentes tipos de clima.
         
@@ -7,13 +7,10 @@ class Clima:
 
         tipo - nome do clima
 
-        bloqueante - Indica se o clima é bloqueante. (bool)
-
-        probabilidade - Indica a probabilidade do clima ocorrer. ([-5,10])
+        probabilidade - Indica a probabilidade do clima ocorrer. ([0,10])
         """
         self.id = id
         self.tipo = tipo
-        self.bloqueante = bloqueante
         self.probabilidade = probabilidade
 
     def __eq__(self, other):
@@ -24,10 +21,13 @@ class Clima:
 
     def isBlocking(self) -> bool:
         """
-        Verifica se o clima é bloqueante. Retorna True se a probabilidade for 100 ou bloquante for True.
+        Verifica se o clima é 10. Retorna True se a probabilidade for 10 e transforma este valor em 0.
         Se probabilidade for 10, bloqueia a zona 1 iteracao
         """
-        return self.probabilidade == 10 or self.bloqueante
+        if self.probabilidade == 10:
+            self.probabilidade = 0
+            return True
+        return False
 
     # Métodos para setar e obter o ID
     def setId(self, id):
@@ -42,13 +42,6 @@ class Clima:
 
     def getTipo(self):
         return self.tipo
-    
-    # Métodos para setar e obter Bloquante
-    def setBloqueante(self, bloqueante):
-        self.bloqueante = bloqueante
-
-    def getBloqueante(self):
-        return self.bloqueante
 
     # Métodos para setar e obter o Probabilidade
     def setProbabilidade(self, probabilidade):
@@ -60,15 +53,15 @@ class Clima:
 
 class Neve(Clima):
     def __init__(self, id, probabilidade = 0):
-        super().__init__(id, tipo="Neve", bloqueante=True, probabilidade=probabilidade)
+        super().__init__(id, tipo="Neve", probabilidade=probabilidade)
 
 
 class Chuva(Clima):
     def __init__(self, id, probabilidade = 0):
-        super().__init__(id, tipo="Chuva", bloqueante=False, probabilidade=probabilidade)
+        super().__init__(id, tipo="Chuva", probabilidade=probabilidade)
 
 
 class Ensolarado(Clima):
     def __init__(self, id, probabilidade = 0):
-        super().__init__(id, tipo="Ensolarado", bloqueante=False, probabilidade=probabilidade)
+        super().__init__(id, tipo="Ensolarado", probabilidade=probabilidade)
 
